a3ba0bd84756f4d9f2addb334854f44d60f6d6d5
Opeyemi0002 <adekoyejoseun@gmail.com>
2025-04-15 17:05:00 +0100

    create user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+}
