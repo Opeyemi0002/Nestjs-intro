2b8eb71bad388a814dd8c238a091591776c061cf
Opeyemi0002 <adekoyejoseun@gmail.com>
2025-04-15 16:37:07 +0100

    configure postgresql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,33 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PostModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
