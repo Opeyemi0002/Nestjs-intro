4eba2551fab4a925af9552087f886b54e1b77664
Opeyemi0002 <adekoyejoseun@gmail.com>
2026-09-05 06:19:15 -0500

    Add E2E tests for empty POST data and successful user registration

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,55 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
 import { AppModule } from 'src/app.module';
+import { MailService } from 'src/mail/mail.service';
+import request from 'supertest';
 
-describe('AppController (e2e)', () => {
+describe('app (e2e)', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  async function createApp() {
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(MailService)
+      .useValue({ sendWelcomeEmail: jest.fn().mockResolvedValue(undefined) })
+      .compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    const app = module.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+
+    return app.init();
+  }
+
+  beforeAll(async () => {
+    app = await createApp();
+  }, 30000);
+  it('should reject registration when no data is provided', async () => {
+    const response = await request(app.getHttpServer()).post('/auth/register');
+    expect(response.status).toBe(400);
+  });
+  it('should', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        firstName: 'Tope',
+        lastName: 'Gbadamosi',
+        email: 'tope@example.com',
+        password: 'Tope-password!',
+      });
+
+    expect(response.status).toBe(201);
   });
 
-  it('/ (GET)', async () => {
-    await request(app.getHttpServer()).get('/').expect(404);
-  });
-
-  afterEach(async () => {
+  afterAll(async () => {
     await app.close();
   });
 });
